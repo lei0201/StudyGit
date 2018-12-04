9d227453165c97ef27f6c48e3a3a909fe53ffab0
lei0201 <Administrator@SC-201806151448>
2018-12-04 16:58:18 +0800

    增加九九乘法表的案例

---
+++ StudyGit/Java基础语法/src/com/term/MultiplicationTable.java
@@ -0,0 +1,18 @@
+package com.term;
+
+public abstract class MultiplicationTable {
+	/**
+	 * 
+	 * @param args
+	 * 九九乘法表
+	 */
+	public static void main(String[] args) {
+		for(int i=1;i<=9;i++) {
+			for(int j=1;j<=i;j++) {
+				System.out.print(i+"*"+j+"="+i*j+"\t");
+			}
+			System.out.println();
+		}
+	}
+
+}
